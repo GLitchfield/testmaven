4627c9c17aeeada6cba243d5a787fa8253fb5913
Gavin Litchfield <gavinm.litchfield@gmail.com>
2012-07-13 08:33:34 -0500

    initial

---
+++ src/main/scala/com/barchart/backend/testmaven/App.scala
@@ -0,0 +1,12 @@
+package com.barchart.backend.testmaven
+
+/**
+ * Hello world!
+ *
+ */
+object App 
+{
+    def main(args: Array[String]):Unit={
+        println( "Hello World!" )
+    }
+}
---
+++ src/test/scala/com/barchart/backend/testmaven/AppTest.scala
@@ -0,0 +1,17 @@
+package com.barchart.backend.testmaven
+
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+/**
+ * Unit test for simple App.
+ */
+class AppTest {
+    /**
+     * Rigourous Test :-)
+     */
+    @Test
+    def testApp():Unit={
+        assertTrue( true )
+    }
+}
